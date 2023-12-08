ec6b3445e0f5cdc28772ee3e3c26aac3996a95d5
Rishi <48rishi@gmail.com>
2023-12-08 23:33:31 +0530

    socket close

--- server.ts
+++ server.ts
@@ -12,80 +12,70 @@ function rand(arr: Array<user>): number {
 }
 
 const waitingList: user[] = [];
-const rooms = [];
+const rooms: Array<user>[] = [];
 
-Deno.serve(
-    { port: 8080 },
-    async function (request: Request): Promise<Response> {
-        if (request.headers.get("upgrade") === "websocket") {
-            const { socket, response } = Deno.upgradeWebSocket(request);
+Deno.serve(async function (request: Request): Promise<Response> {
+    if (request.headers.get("upgrade") === "websocket") {
+        const { socket, response } = Deno.upgradeWebSocket(request);
 
-            socket.addEventListener("open", () => {
-                // const id = uuid();
-                // console.log(id);
-                // socket.send(JSON.stringify({ myid: id }));
-                // waitingList[id] = socket;
-                // const l = Object.keys(waitingList).length;
-                // if (l > 2) {
-                //     let id1 = getrand(waitingList);
-                //     let id2 = getrand(waitingList);
-                //     rooms.push([
-                //         {
-                //             [id1]: waitingList[id1],
-                //             [id2]: waitingList[id2],
-                //         },
-                //     ]);
-                //     waitingList[id1].send(JSON.stringify({ youid: id2 }));
-                //     waitingList[id2].send(JSON.stringify({ youid: id1 }));
-                //     delete waitingList[id1], waitingList[id2];
-                //     console.log("shifted to room");
-                //     console.log(Object.keys(waitingList).length);
-                // }4
-                waitingList.forEach((user) => {
-                    console.log("sent");
-                    user.socket.send(JSON.stringify({ myid: user.id }));
+        socket.addEventListener("open", () => {
+            waitingList.forEach((user) => {
+                console.log("sent");
+                user.socket.send(JSON.stringify({ myid: user.id }));
+            });
+        });
+
+        socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            console.log(data);
+            if (data.myid) {
+                waitingList.push({
+                    id: data.myid,
+                    socket,
                 });
-            });
+            }
 
-            socket.onmessage = (event) => {
-                const data = JSON.parse(event.data);
-                console.log(data);
-                if (data.myid) {
-                    waitingList.push({
-                        id: data.myid,
-                        socket,
-                    });
+            const l = waitingList.length;
+            if (l > 1) {
+                const i1 = rand(waitingList);
+                const i2 = rand(waitingList);
+                const u1 = waitingList[i1];
+                const u2 = waitingList[i2];
+                rooms.push([u1, u2]);
+                u1.socket.send(JSON.stringify({ youid: u2.id }));
+                // u2.socket.send(JSON.stringify({ youid: u1.id }));
+                waitingList.splice(i1, 1);
+                waitingList.splice(i2, 1);
+            }
+        };
+
+        socket.addEventListener("close", () => {
+            for (let i = 0; i < waitingList.length; ++i) {
+                if (waitingList[i].socket === socket) waitingList.splice(i, 1);
+            }
+
+            for (let i = 0; i < rooms.length; ++i) {
+                if (rooms[i][0].socket === socket) {
+                    waitingList.push(rooms[i][1]);
+                    rooms.splice(i, 1);
+                } else if (rooms[i][1].socket === socket) {
+                    waitingList.push(rooms[i][1]);
+                    rooms.splice(i, 1);
                 }
+            }
+            console.log("closed");
+        });
 
-                const l = waitingList.length;
-                if (l > 2) {
-                    const i1 = rand(waitingList);
-                    const i2 = rand(waitingList);
-                    const u1 = waitingList[i1];
-                    const u2 = waitingList[i2];
-                    rooms.push([u1, u2]);
-                    u1.socket.send(JSON.stringify({ youid: u2.id }));
-                    // u2.socket.send(JSON.stringify({ youid: u1.id }));
-                    waitingList.splice(i1, 1);
-                    waitingList.splice(i2, 1);
-                }
-            };
-
-            socket.addEventListener("close", () => {
-                console.log("closed");
-            });
-
-            return response;
-        } else if (new URLPattern({ pathname: "/" }).exec(request.url)) {
-            return new Response(
-                await Deno.readTextFile("./client/index.html").catch((o) => o),
-                {
-                    status: 200,
-                    headers: { "content-type": "text/html" },
-                }
-            );
-        }
-
-        return new Response("Not found! go to home", { status: 404 });
+        return response;
+    } else if (new URLPattern({ pathname: "/" }).exec(request.url)) {
+        return new Response(
+            await Deno.readTextFile("./client/index.html").catch((o) => o),
+            {
+                status: 200,
+                headers: { "content-type": "text/html" },
+            }
+        );
     }
-);
+
+    return new Response("Not found! go to home", { status: 404 });
+});
