97d7dc159d7927b7101e667fcd82ac963a31113a
rishift <144378316+rishift@users.noreply.github.com>
2023-12-21 15:46:12 +0530

    minor changes

--- client/main.js
+++ client/main.js
@@ -8,7 +8,6 @@ const peerConn = new RTCPeerConnection({
         urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
       },
     ],
-    iceCandidatePoolSize: 10,
   });
 
 let localStream;
